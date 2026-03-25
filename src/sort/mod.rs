7e9bb017f886cb1397556adafbbb512ec9c3d64a
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-03-25 23:50:28 +0430

    feat: create comparison.rs and transfer the insertion_sort() into comparison.rs

--- src/sort/mod.rs
+++ src/sort/mod.rs
@@ -1,6 +1,7 @@
 pub mod simple_sort;
+pub mod comparison;
 
 // Re-export for easier access
-pub use simple_sort::insertion_sort;
+pub use comparison::insertion_sort;
 pub use simple_sort::bubble_sort;
 pub use simple_sort::selection_sort;
