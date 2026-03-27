70d78b25f79bf2397fb207152312a0fafa1fde52
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-03-27 16:39:57 +0430

    rafactor: add comparison modul to the sort modul and remove unneccesary comparison.rs from src/sort/

--- src/sort/mod.rs
+++ src/sort/mod.rs
@@ -2,7 +2,6 @@ pub mod simple_sort;
 pub mod comparison;
 
 // Re-export for easier access
-pub use comparison::insertion_sort;
-pub use comparison::merge_sort;
-pub use simple_sort::bubble_sort;
+pub use comparison::insertion::insertion_sort;
+pub use comparison::bubble::bubble_sort;
 pub use simple_sort::selection_sort;
